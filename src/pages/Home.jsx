f9ebdb301677244e8815787aaf4c012e9c304a4e
Emmanuel Castillo <emmanuel.2001alonso.ec@gmail.com>
2023-07-26 11:54:37 -0500

    react prep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate()
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (
+            <div className="user" key={user.id} onClick={() => navigate(`${user.id}`)}>
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>{user.name}</h3>
+                  <p>
+                    <b>Email:</b> {user.email}
+                  </p>
+                  <p>
+                    <b>Phone:</b> {user.phone}
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    {user.website}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
